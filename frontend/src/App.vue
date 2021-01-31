d5f3d49ae77bf01c6c5b5f4869f4508574ea84e6
Eezi <tomi.a.salo@outlook.com>
2021-01-31 18:53:51 +0200

    Added canvas and moveing keys for frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <BlockGame />
+  </div>
+</template>
+
+<script>
+import BlockGame from './components/BlockGame.vue'
+
+export default {
+  name: 'App',
+  components: {
+    BlockGame 
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  background: #ddd;
+  height: 100vh;
+}
+</style>
